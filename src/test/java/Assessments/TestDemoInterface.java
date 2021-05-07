7633dd7894e04f5e235108590f71b8720082e2a5
tim15os <tim.dzhigkaev@gmail.com>
2021-05-07 13:33:35 -0500

    renamed class

--- src/test/java/Assessments/TestDemoInterface.java
+++ src/test/java/Assessments/TestDemoInterface.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 
 
-public class TestDemoIntrface {
+public class TestDemoInterface {
 
     public static void main(String[] args) {
         List<Car> carList = createCarList();
