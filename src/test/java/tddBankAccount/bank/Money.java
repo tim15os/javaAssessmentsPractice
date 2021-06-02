7ece35cd532a39a84e3235804e39e7edd082fa70
timur15os <tim.dzhigkaev@gmail.com>
2021-06-01 20:41:02 -0500

    added tdd bankAccountApplication and change the packages name

---
+++ src/test/java/tddBankAccount/bank/Money.java
@@ -0,0 +1,45 @@
+package tddBankAccount.bank;
+
+import java.math.BigDecimal;
+
+/**
+ * Immutable class to represent Money as a concept.
+ * This class should have no accessor methods.
+ */
+public class Money {
+    private final BigDecimal amount;
+
+    public Money() {
+        this(BigDecimal.ZERO);
+    }
+
+    public Money(String amount) {
+        this(new BigDecimal(amount));
+    }
+
+    public Money(BigDecimal amount) {
+        if (amount.signum() < 0) throw new IllegalArgumentException("The amount cannot be negative.");
+        this.amount = amount;
+    }
+
+    protected static Money getSum(Money one, Money two) {
+        return new Money(one.amount.add(two.amount));
+    }
+
+    protected static Money getDifference(Money one, Money two) {
+        return new Money(one.amount.subtract(two.amount));
+    }
+
+    protected static Integer compare(Money one, Money two) {
+        return one.amount.compareTo(two.amount);
+    }
+
+    protected static Boolean isPositiveAmount(Money money) {
+        return money.amount.signum() > 0;
+    }
+
+    @Override
+    public String toString() {
+        return this.amount.toString();
+    }
+}
